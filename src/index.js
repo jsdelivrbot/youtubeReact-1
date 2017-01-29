4e6dcb5135bfe012bafc1a8d715ac89edca2edfa
Marc Fournier <marc.fournier91@gmail.com>
2017-01-28 22:27:59 -0500

    Search bar input value changes to new state onchange. YT api function tested with sample search.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import API_KEY from './APIKey'; //file is gitignored
+import SearchBar from './components/search_bar';
+
+YTSearch({key: API_KEY, term: 'mozart'}, function(data){ //test the api with dummy search
+  console.log(data);
+});
+
 //create a new component which should produce html
 const App = () => {
-  return <div>Jello</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 console.log(API_KEY);
 //take component's generated html and put it in the dom
